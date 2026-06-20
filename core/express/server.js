897f7b325133d2c0db7ca26d3cc9b0aeff6b834f
NYC! <neerajyamaji@gmail.com>
2026-06-20 16:54:58 +0530

    fix: error code mapping and response body structure for custom errors

--- core/express/server.js
+++ core/express/server.js
@@ -243,6 +243,7 @@ function Server(serverConfig = {}) {
 
         responseComponents.statusCode = statusCode;
         responseComponents.body.status = 'error';
+        responseComponents.body.error_code = error.errorCode;
         responseComponents.body.message = error.isApplicationError
           ? error.message
           : 'Some error occured.';
